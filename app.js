4521d2d5983f5d943d5a6f0652e9659eda20e8a2
petchy <petch.it24@windowslive.com>
2020-03-28 18:25:34 +0700

    Condition v-if

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 new Vue({
   el:"#vue-app",
   data:{
-    name:"Petch Akkarapol",
-    job:"",
-    age:34,
-    status:false,
-    website: "https://www.youtube.com",
-    count:0,
-    foods:[],
-    gender:''
+    like:false
   },
   methods:{
-    getName:function(){
-      return this.name
-    },
-    setName:function(name){
-      return this.name = name
-    },
-    addAge:function(){
-      console.log(++this.age)
-    },
-    subtrackAge:function(){
-      console.log(--this.age)
-    },
-    addCount:function(){
-      console.log(this.count++)
-    }
   }
 })
