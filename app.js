96328c8acc0af8426d0cead2867d0d1bcde28134
petchy <petch.it24@windowslive.com>
2020-03-28 18:40:11 +0700

    Loop v-for

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 new Vue({
   el:"#vue-app",
   data:{
-    like:false
+    like:false,
+    products: ["Coffee", "Beer", "Coke"],
+    employees: [
+      {name: "Jesica", age: 20},
+      {name: "Peter", age: 25}
+    ]
   },
   methods:{
   }
