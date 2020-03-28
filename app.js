9d1f13ef27c8278131042223664af14da6cf7e0c
petchy <petch.it24@windowslive.com>
2020-03-28 23:39:43 +0700

    Multiple Vue

--- app.js
+++ app.js
@@ -1,46 +1,21 @@
-new Vue({
-  el:"#vue-app",
+var post = new Vue({
+  el:"#post",
   data:{
-    newData:{
-      name:"",
-      salary:0
-    },
-    employees: [
-      {name: "Jesica", salary: 20000},
-      {name: "Peter", salary: 25000}
-    ]
-  },
-  methods:{
-    addEmployee:function(){
-      this.employees.push({
-        name: this.newData.name,
-        salary: this.newData.salary
-      })
-      this.newData.name = '';
-      this.newData.salary = 0;
-    },
-    showMessage:function(){
-      console.log("Send data...");
-    }
-  },
-  computed:{
-    salarySummary:function(){
-      var summary = this.employees.reduce(function(value, data) {
-        return value + Number(data.salary);
-      }, 0);
-      return summary;
-    },
-    average:function(){
-      var summary = this.employees.reduce(function(value, data) {
-        return value + Number(data.salary);
-      }, 0);
-      return summary/this.employees.length;
-    }
-  },
-  watch:{
-    //after salarySummary call showMessage
-    salarySummary:function(){
-      this.showMessage();
-    }
+    title: "Covid-19",
+    message: "Choose correctly! How to use each type of hygienic mask"
   }
-})
+});
+
+var comment = new Vue({
+  el:"#comment",
+  data:{
+    comment: "As we all know that nowadays Covid19 is spreading around very quickly. People are more aware of how to protect themselves. The easiest way to protect themselves is to wear a hygienic mask. But do you know there are 4 types of hygienic masks and are you sure that the one you are wearing is correct"
+  }
+});
+
+var like = new Vue({
+  el:"#likeSystem",
+  data:{
+    like:false
+  }
+});
